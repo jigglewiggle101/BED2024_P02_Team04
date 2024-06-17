4884e4e9a9ffb73e12fbb3c9c629ea75bc39bb74
Aiman <s10258376@connect.np.edu.sg>
2024-06-17 20:04:13 +0800

    user id validation change from string to int

--- asiaprepared/asiaprepared-api-mvc-db/middlewares/validateUser.js
+++ asiaprepared/asiaprepared-api-mvc-db/middlewares/validateUser.js
@@ -1,11 +1,12 @@
 const Joi = require("joi"); // Use uppercase 'Joi' when requiring the module
 
 const validateUser = (req, res, next) => {
-  const schema = Joi.object({
-    id: Joi.string().min(3).max(50).required(),
-    username: Joi.string().min(3).max(50).required(),
-    email: Joi.string().min(3).max(50).required()
-  });
+    const schema = Joi.object({
+      id: Joi.number().integer().min(4).required(), // Changed to integer
+      name: Joi.string().min(3).max(50),
+      email: Joi.string().min(3).max(255),
+      password: Joi.string().min(3).max(255).required()
+    });
 
   const validation = schema.validate(req.body, { abortEarly: false });
 
