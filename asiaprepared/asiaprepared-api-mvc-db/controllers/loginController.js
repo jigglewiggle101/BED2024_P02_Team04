04f71cb53bd018aa2bfdbfd3afc04f45e4d8322e
kaijieleong <s10262480@connect.np.edu.sg>
2024-06-19 15:57:09 +0800

    Updated Login
    Login models and controller is used for login and sign up

--- asiaprepared/asiaprepared-api-mvc-db/controllers/loginController.js
+++ asiaprepared/asiaprepared-api-mvc-db/controllers/loginController.js
@@ -1,33 +1,42 @@
 const Login = require("../models/login");
 
 const createUser = async (req, res) => {
-    const newUser = req.body;
-    try {
-      const createdUser = await Login.createUser(newUser);
-      res.status(201).json(createdUser);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Error creating user");
-    }
-  };
-  
-  const updateUser = async (req, res) => {
-    const userId = parseInt(req.params.id);
-    const newUserData = req.body;
-  
-    try {
-      const updatedUser = await Login.updateUser(userId, newUserData);
-      if (!updatedUser) {
-        return res.status(404).send("User not found");
-      }
-      res.json(updatedUser);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Error updating user");
-    }
-  };
+  const newUser = req.body;
+  try {
+    const createdUser = await Login.createUser(newUser);
+    res.status(201).json(createdUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error creating user");
+  }
+};
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const loginResult = await UserModel.loginUser(username, password);
 
-  module.exports = {
-    createUser,
-    updateUser,
-  };
+    if (loginResult.success) {
+      res.status(200).json({
+        success: true,
+        message: loginResult.message,
+        user: loginResult.user,
+      });
+    } else {
+      res.status(401).json({
+        success: false,
+        message: loginResult.message,
+      });
+    }
+  } catch (error) {
+    console.error("Error in login:", error.message);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+module.exports = {
+  createUser,
+  login,
+};
--- asiaprepared/asiaprepared-api-mvc-db/models/login.js
+++ asiaprepared/asiaprepared-api-mvc-db/models/login.js
@@ -2,43 +2,73 @@ const sql = require("mssql");
 const dbConfig = require("../dbConfig");
 
 class Login {
-    constructor(id, name, email, password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
+  constructor(id, name, email, password) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+  static async createUser(newUserData) {
+    const connection = await sql.connect(dbConfig);
+
+    const sqlQuery = `
+            INSERT INTO UserAcc (Username, UserContactNo, Useremail, UserPass)
+            VALUES (@username, @contact, @email, @password);
+            SELECT SCOPE_IDENTITY() AS id;
+        `;
+
+    const request = connection.request();
+
+    request.input("username", newUserData.username);
+    request.input("password", newUserData.password);
+    if (newUserData.contact) {
+      request.input("contact", newUserData.contact);
+    } else {
+      request.input("contact", null);
     }
-    static async createUser(newUserData) {
-        const connection = await sql.connect(dbConfig);
 
-        const sqlQuery = `INSERT INTO Users (name, email) VALUES (@username, @email); SELECT SCOPE_IDENTITY() As id;`;
-
-        const request = connection.request();
-
-        request.input("username", newUserData.username);
-        request.input("email", newUserData.email);
-        request.input("password", newUserData.password)
-
-        const result = await request.query(sqlQuery);
-
-        connection.close;
-
+    if (newUserData.email) {
+      request.input("email", newUserData.email);
+    } else {
+      request.input("email", null);
     }
-    static async updateUser(id, newUserData) {
-        const connection = await sql.connect(dbConfig);
-        const sqlQuery = `UPDATE Users SET username = @username, email = @email WHERE id = @id`; // Parameterized query
+    const result = await request.query(sqlQuery);
 
-        const request = connection.request();
-        request.input("id", id);
-        request.input("username", newUserData.username || null); // Handle optional fields
-        request.input("email", newUserData.email || null);
+    connection.close();
+  }
+  static async loginUser(username, password) {
+    try {
+      await sql.connect(dbConfig);
+      const sqlQuery = `
+            SELECT UserID, Username
+            FROM UserAcc
+            WHERE Username = @username AND UserPass = @password;
+        `;
+      const request = new sql.Request();
+      request.input("username", username);
+      request.input("password", password);
 
-        await request.query(sqlQuery);
+      const result = await request.query(sqlQuery);
+      sql.close();
 
-        connection.close();
-
-        return this.getUserById(id); // returning the updated book data
+      if (result.recordset.length > 0) {
+        const user = result.recordset[0];
+        return {
+          success: true,
+          message: "Login successful",
+          user: user,
+        };
+      } else {
+        return {
+          success: false,
+          message: "Invalid username or password",
+        };
+      }
+    } catch (err) {
+      console.error("Error logging in:", err.message);
+      throw err;
     }
+  }
 }
 
 module.exports = Login;
