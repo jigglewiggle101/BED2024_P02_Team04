89e92042a6b22c505670a4d309f7b52705668d3e
jigglewiggle101 <s10262431@connect.np.edu.sg>
2024-06-27 22:40:38 +0800

    updated dbconfig with necessary credentials

--- dbConfig.js
+++ dbConfig.js
@@ -1,13 +1,12 @@
 //db config
-
 module.exports = {
-    user: "", // Replace with your SQL Server login username
-    password: "", // Replace with your SQL Server login password
-    server: "localhost",
-    database: "bed_db", //Replace with actual db name
-    trustServerCertificate: true,
-    options: {
+  user: "Apr_User", // Replace with your SQL Server login username
+  password: "Scrum@you", // Replace with your SQL Server login password
+  server: "localhost",
+  database: "bed_db",
+  trustServerCertificate: true,
+  options: {
       port: 1433, // Default SQL Server port
       connectionTimeout: 60000, // Connection timeout in milliseconds
-    },
-  };
+  },
+};
