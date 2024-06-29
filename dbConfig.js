6a7ab565f59d1cafbaf220cb4c5543b8fc0decce
jigglewiggle101 <s10262431@connect.np.edu.sg>
2024-06-29 22:28:46 +0800

    Update files

---
+++ dbConfig.js
@@ -0,0 +1,13 @@
+//db config
+module.exports = {
+  user: "Apr_User", // Replace with your SQL Server login username
+  password: "Scrum@you", // Replace with your SQL Server login password
+  server: "localhost",
+  database: "APR_BED",
+  trustServerCertificate: true,
+  options: {
+      port: 1433, // Default SQL Server port
+      connectionTimeout: 60000, // Connection timeout in milliseconds
+  },
+};
+
