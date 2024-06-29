461ac0a6a9098fb79598b6a852093136d69475be
Aiman <s10258376@connect.np.edu.sg>
2024-06-29 18:12:54 +0800

    dbConfig change and added a bcrypt module for encryption of password

--- asiaprepared/asiaprepared-api-mvc-db/dbConfig.js
+++ asiaprepared/asiaprepared-api-mvc-db/dbConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   user: "Apr_User", // Replace with your SQL Server login username
   password: "Scrum@you", // Replace with your SQL Server login password
   server: "localhost",
-  database: "bed_db",
+  database: "Apr_BED",
   trustServerCertificate: true,
   options: {
       port: 1433, // Default SQL Server port
